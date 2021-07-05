4836c5eb1425b23b9ba48062c8a965cb73f32f39
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2021-07-05 11:47:03 +0800

    markdown loader broken

--- src/blogs/BlogImport.js
+++ src/blogs/BlogImport.js
@@ -1,9 +1,10 @@
 
 
-import React, { Component } from 'react';
-import AppMarkdown from './ffestiniog.md';
+import React, { Component, useState, useEffect } from 'react';
+import AppMarkdown from '!!html-loader!./ffestiniog.md';
 import ReactMarkdown from 'react-markdown';
 
+/*
 class BlogImport extends Component {
   constructor() {
     super();
@@ -19,6 +20,35 @@ class BlogImport extends Component {
     const { markdown } = this.state;
     return <ReactMarkdown source={markdown} />;
   }
+}
+*/
+
+function BlogImport() {
+
+    // const file_name = './Blogging/blogs/example-blog/template.md';
+    const [post, setPost] = useState('');
+
+    
+    useEffect(() => {
+        import(`${file_name}`)
+            .then(res => {
+                fetch(res.default)
+                    .then(res => res.text())
+                    .then(res => setPost(res))
+                    .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
+    });
+
+    return (
+        <div class="align-items-md-stretch h-100 p-5">
+            <ReactMarkdown remarkPlugins={[gfm]} children={post} />
+        </div>
+    );
+    
+
+    //return{ __html: 'Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello'};
+
 }
 
 export default BlogImport;
