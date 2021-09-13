c2363672a25519d7813aed547dc37c1365afd2fe
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2021-09-13 13:05:10 +0100

    Font Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import Image from 'react-bootstrap/Image'
 import { ytc_links } from '../components/Links';
 import PageTitle from '../components/PageTitle';
 
+import './Home.css'
+
 
 function HomePage() {
 
@@ -27,7 +29,7 @@ function HomePage() {
                 </div>
             </div>
 
-            <h1 class="display-5 fw-bold mt-4">Engineer, Rail Enthusiast, and Jaywalker.</h1>
+            <h1 class="display-5 fw-bold mt-4 roboto">Engineer, Rail Enthusiast, and Jaywalker.</h1>
             <div class="col-lg-6 mx-auto container py-4">
                 <p class="lead mt-4">
                     Hello! I am a Electrical and Electronic Engineering undergraduate at Imperial College London, with attestable internship and volunteering experiences. I have developed a profound interest for trains, in particular the sophisticated rolling stock traction motors, and the intricate logistics involved in the industry's operation. I also enjoy "jaywalking" and documenting these experiences. Afterall, "the world is my oyster"!
