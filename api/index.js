7ec9079906e324be3b6af33e54c5adfdb5283f94
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2021-10-06 15:00:03 +0100

    cannot deploy to heroku

--- api/index.js
+++ api/index.js
@@ -1,43 +1,35 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+const cors =  require('cors');
+
 // const authRoute = require("./routes/auth");
 // const userRoute = require("./routes/users");
 const projectPostRoute = require("./routes/projectPosts");
 // const categoryRoute = require("./routes/categories");
-const multer = require("multer");
-const path = require("path");
+
+const app = express();
 
 dotenv.config();
 app.use(express.json());
-//app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors());
+
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/5000';
+const PORT  = 3000;
 
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify:true
+    useFindAndModify: true
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
 /*
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/categories", categoryRoute);
@@ -45,6 +37,11 @@ app.use("/api/categories", categoryRoute);
 
 app.use("/api/projectposts", projectPostRoute);
 
-app.listen("5000", () => {
-  console.log("Backend is running.");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Backend is running, ${process.env.PORT}.`);
 });
+
+app.get('/', (req, res) => {
+  res.status(200).send('hello world')
+});
+
