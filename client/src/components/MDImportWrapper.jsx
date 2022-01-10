7e4e02de00fdf2e8e18917739957090211093fe3
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2022-01-10 13:44:10 +0000

    First commit

--- client/src/components/MDImportWrapper.jsx
+++ client/src/components/MDImportWrapper.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import MDImport from "./MDImport";
-
 import SocialMediaShare from "../components/SocialMediaShare";
+
+import { FacebookProvider, Like } from 'react-facebook';
 
 class MDImportWrapper extends Component {
 
@@ -37,6 +38,14 @@ class MDImportWrapper extends Component {
                 <div class="container col-md-10 col-lg-8 cust-title mt-3 mb-3 display-4">
                     <SocialMediaShare link={this.props.location.pathname} />
                 </div>
+                {/* <div class="fb-comments"
+                    data-href="https://localhost:3000/ytc-web#/blogs/mogodB/Pretty%20needed%20Chill/61d33d9f69f66a56c65c3486"
+                    data-width="" 
+                    data-numposts="5"
+                ></div> */}
+                <FacebookProvider appId="215081200832412">        
+                <Like href="http://www.facebook.com" colorScheme="dark" showFaces share />
+                </FacebookProvider>
                 <MDImport title={this.state.title} id={this.state.id} />
             </div>
         );
