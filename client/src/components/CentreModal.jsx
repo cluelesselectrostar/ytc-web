5cbec3c47b58a018eb2999873835337b566a2003
cluelesselectrostar <ytc19@ic.ac.uk>
2022-05-29 00:34:32 +0100

    modal on click

---
+++ client/src/components/CentreModal.jsx
@@ -0,0 +1,27 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
+function CentreModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {props.content}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default CentreModal;
--- client/src/travel/Woodstock.jsx
+++ client/src/travel/Woodstock.jsx
@@ -4,15 +4,13 @@ import PageTitle from '../components/PageTitle';
 import React, { useRef, useEffect, useState } from "react";
 //import { SvgLoader, SvgProxy } from 'react-svgmt';
 import Alert from 'react-bootstrap/Alert';
-import Button from "react-bootstrap/Button";
+import CentreModal from '../components/CentreModal';
+
 import * as d3 from "d3";
-import ReactTooltip from 'react-tooltip';
 
 import './Woodstock.css';
 
-//import OptimisedSVG from './optimised.svg';
 //import SvgOriginalMap from "./optimised_map";
-//import {ReactComponent as MapSVG} from './official_tfl.svg';
 import { ReactComponent as MapSVG } from './original_map.svg';
 
 
@@ -23,6 +21,12 @@ function WoodstockTravels() {
     const svgRef = useRef();
     const [selectedStation, setSelectedStation] = useState("Hover over a station for details!");
     const [isMobile, setMobile] = useState(false);
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    var list = [];
 
     useEffect(() => {
 
@@ -35,25 +39,50 @@ function WoodstockTravels() {
         var svg = d3.select(svgRef.current);
         var attr = "";
 
-        //svg.selectAll("text").attr("font-size", "10px");
+        // svg.selectAll("text").attr("font-size", "10px");
         // svg.selectAll("use").attr("stroke-width",50); //testing what "use" contains
+
+        svg.selectAll("text")
+            .filter(function (d) {
+                console.log(d3.select(this)._groups[0][0].innerHTML);
+                attr = d3.select(this)._groups[0][0].innerHTML;
+                return attr.includes("&amp;");
+            }).append("circle")
+
+        svg.selectAll("use")
+            .filter(function (d) {
+                attr = d3.select(this)._groups[0][0].getAttributeNS('http://www.w3.org/1999/xlink', 'href');
+                list.push(d3.select(this)._groups[0][0].id);
+                console.log(list);
+                return attr.includes("#st") || attr.includes("wheelchair") || attr.includes("base") || attr.includes("crutch") || attr.includes("int") || attr.includes("cap") || attr.includes("term");
+            })
+            .on("mouseover", datum => {
+                //console.log("hovered feature");
+                //console.log(datum.srcElement.id);
+                //console.log(datum.srcElement.href.baseVal);
+                if (datum.srcElement.id != "") {
+                    setSelectedStation(datum.srcElement.id);
+                }
+            })
+            .style("cursor", "pointer"); 
 
         svg.selectAll("use")
             .filter(function (d) {
                 attr = d3.select(this)._groups[0][0].getAttributeNS('http://www.w3.org/1999/xlink', 'href');
                 return attr.includes("#st") || attr.includes("wheelchair") || attr.includes("base") || attr.includes("crutch") || attr.includes("int") || attr.includes("cap") || attr.includes("term");
             })
-            .on("mouseover", datum => { // on click to do other things?
-                console.log("hovered feature");
+            .on("click", datum => {
+                //console.log("clicked feature");
                 //console.log(datum);
-                console.log(datum.srcElement.id);
-                console.log(datum.srcElement.href.baseVal);
-                d3.select(this).append("div").html("FIRST LINE <br> SECOND LINE");
-
+                //console.log(datum.srcElement.id);
+                //console.log(datum.srcElement.href.baseVal);
                 if (datum.srcElement.id != "") {
                     setSelectedStation(datum.srcElement.id);
+                    handleShow();
                 }
             })
+            .style("cursor", "pointer"); 
+
     }, []);
 
 
@@ -65,13 +94,20 @@ function WoodstockTravels() {
                 description="Woodstock hitching on Snoopy's rides!"
             />
             {isMobile ?
-                <Alert variant="success" style={{ position: "sticky", margin: "5px", left: 0, top: "1%" }}>{selectedStation}</Alert>
+                <Alert variant="success" style={{ position: "sticky", margin: "5px", left:"1%", top: "1%" }}>{selectedStation}</Alert>
                 :
-                <Alert variant="success" style={{ position: "sticky", margin: "5px", left: 0, top: "1%", width: "300px" }}>{selectedStation}</Alert>
+                <Alert variant="success" style={{ position: "sticky", margin: "5px", left:"1%", top: "1%", width: "300px" }}>{selectedStation}</Alert>
             }
             <div>
                 <MapSVG ref={svgRef} />
             </div >
+
+            <CentreModal
+                title="Station name"
+                content="Woodstock has not been here before!"
+                show={show}
+                onHide={() => setShow(false)}
+            />
         </main>
     );
 }
