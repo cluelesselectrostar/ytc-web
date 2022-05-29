0360de08f33c993381964f0cd784027331c53e21
cluelesselectrostar <ytc19@ic.ac.uk>
2022-05-30 00:27:02 +0100

    bank monument hub fixed

--- client/src/travel/Woodstock.jsx
+++ client/src/travel/Woodstock.jsx
@@ -26,6 +26,17 @@ function WoodstockTravels() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    var attr = "";
+
+    // FILTER FUNCTIONS
+
+    const filter_stations = (input) => {
+        attr = d3.select(input)._groups[0][0].getAttributeNS('http://www.w3.org/1999/xlink', 'href');
+        list.push(d3.select(input)._groups[0][0].id);
+        //console.log(list);
+        return attr.includes("#st") || attr.includes("wheelchair") || attr.includes("base") || attr.includes("crutch") || attr.includes("int") || attr.includes("cap") || attr.includes("term") || attr.includes("hub");
+    };
+
     var list = [];
 
     useEffect(() => {
@@ -37,51 +48,42 @@ function WoodstockTravels() {
         }
 
         var svg = d3.select(svgRef.current);
-        var attr = "";
 
         // svg.selectAll("text").attr("font-size", "10px");
         // svg.selectAll("use").attr("stroke-width",50); //testing what "use" contains
 
-        svg.selectAll("text")
-            .filter(function (d) {
-                console.log(d3.select(this)._groups[0][0].innerHTML);
-                attr = d3.select(this)._groups[0][0].innerHTML;
-                return attr.includes("&amp;");
-            }).append("circle")
-
         svg.selectAll("use")
             .filter(function (d) {
-                attr = d3.select(this)._groups[0][0].getAttributeNS('http://www.w3.org/1999/xlink', 'href');
-                list.push(d3.select(this)._groups[0][0].id);
-                console.log(list);
-                return attr.includes("#st") || attr.includes("wheelchair") || attr.includes("base") || attr.includes("crutch") || attr.includes("int") || attr.includes("cap") || attr.includes("term");
+                return filter_stations(this);
             })
             .on("mouseover", datum => {
-                //console.log("hovered feature");
-                //console.log(datum.srcElement.id);
-                //console.log(datum.srcElement.href.baseVal);
-                if (datum.srcElement.id != "") {
+                //console.log("hovered feature id:" + datum.srcElement.id + ", baseVal:" + datum.srcElement.href.baseVal);
+                if (datum.srcElement.id !== "") {
                     setSelectedStation(datum.srcElement.id);
                 }
             })
-            .style("cursor", "pointer"); 
-
-        svg.selectAll("use")
-            .filter(function (d) {
-                attr = d3.select(this)._groups[0][0].getAttributeNS('http://www.w3.org/1999/xlink', 'href');
-                return attr.includes("#st") || attr.includes("wheelchair") || attr.includes("base") || attr.includes("crutch") || attr.includes("int") || attr.includes("cap") || attr.includes("term");
-            })
             .on("click", datum => {
-                //console.log("clicked feature");
-                //console.log(datum);
-                //console.log(datum.srcElement.id);
-                //console.log(datum.srcElement.href.baseVal);
-                if (datum.srcElement.id != "") {
+                if (datum.srcElement.id !== "") {
                     setSelectedStation(datum.srcElement.id);
                     handleShow();
                 }
             })
-            .style("cursor", "pointer"); 
+            .style("cursor", "pointer");
+
+        svg.selectAll("#Bank_Monument_hub")
+            .on("mouseover", datum => {
+                if (datum.srcElement.id !== "") {
+                    console.log("Hovered over bank monument hub")
+                    setSelectedStation(datum.srcElement.id);
+                }
+            })
+            .on("click", datum => {
+                if (datum.srcElement.id !== "") {
+                    setSelectedStation(datum.srcElement.id);
+                    handleShow();
+                }
+            })
+            .style("cursor", "pointer");
 
     }, []);
 
@@ -93,11 +95,13 @@ function WoodstockTravels() {
                 title="Woodstock Travels! 🤙"
                 description="Woodstock hitching on Snoopy's rides!"
             />
-            {isMobile ?
-                <Alert variant="success" style={{ position: "sticky", margin: "5px", left:"1%", top: "1%" }}>{selectedStation}</Alert>
+
+            {/* {isMobile ?
+                <Alert variant="success" style={{ position: "sticky", margin: "5px", left: "1%", top: "1%" }}>{selectedStation}</Alert>
                 :
-                <Alert variant="success" style={{ position: "sticky", margin: "5px", left:"1%", top: "1%", width: "300px" }}>{selectedStation}</Alert>
-            }
+                <Alert variant="success" style={{ position: "sticky", margin: "5px", left: "1%", top: "1%", width: "300px" }}>{selectedStation}</Alert>
+            } */}
+
             <div>
                 <MapSVG ref={svgRef} />
             </div >
